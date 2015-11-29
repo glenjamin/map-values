493f9bb58f332665515046a0bab3eb01d74dffac
Glen Mailer <glenjamin@gmail.com>
2015-11-29 19:14:50 +0000

    Use transients if possible

--- src/handy/map_values.clj
+++ src/handy/map_values.clj
@@ -1,9 +1,16 @@
 (ns handy.map-values)
 
+(def ^:private empty-transient (comp transient empty))
+
+(defn- reduce-values [f op init coll]
+  (reduce-kv
+   (fn [m k v] (op m k (f v)))
+   init coll))
+
 (defn map-values
   "Apply f to each value in the key-value coll, preserving keys"
   [f coll]
   {:pre [(satisfies? clojure.core.protocols/IKVReduce coll)]}
-  (reduce-kv
-   (fn [m k v] (assoc m k (f v)))
-   (empty coll) coll))
+  (if (instance? clojure.lang.IEditableCollection coll)
+    (persistent! (reduce-values f assoc! (empty-transient coll) coll))
+    (reduce-values f assoc (empty coll) coll)))
