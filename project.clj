0d55fbc80f086b8c2e400e84cb39b4d0192f3e97
Glen Mailer <glenjamin@gmail.com>
2016-04-22 11:23:58 +0100

    1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject handy/map-values "1.0.0"
+(defproject handy/map-values "1.0.1"
   :description "A one-function clojure lib that provides (map-values)"
   :url "https://github.com/glenjamin/map-values"
   :license {:name "MIT" :url "https://opensource.org/licenses/MIT"}
